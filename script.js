bedf6e5937df9022f3400a5e6adc2399b9243b99
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-06-04 12:26:00 -0300

    incluindo um pouco mais de javascript

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function menuShow(){
         document.querySelector('.icon').src = "./remove.png"
     }
 }
+
+function alert(){
+    Swal.fire({
+        title: "CODETECH",
+        text: "Você está prestes a entrar em um mundo onde o oceano é protegido e o futuro é azul.",
+        icon: "success"
+    });
+}
+
+
+function copy(){
+    Swal.fire({
+        title: "COPYRIGHT",
+        text: "O copyright, também conhecido como direito autoral, é um conjunto de direitos legais concedidos ao criador de uma obra original, como livros, músicas, filmes, software, e outras formas de expressão criativa.",
+        icon: ""
+    });
+}
+
