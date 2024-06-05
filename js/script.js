40729b0fe6be1ec9314a9ba6679e44fe7f05b117
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-06-05 09:48:01 -0300

    ajuste de caminho de imagem js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ function menuShow(){
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "./menu.png"
+        document.querySelector('.icon').src = "./imagens/menu.png"
     }else{
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "./remove.png"
+        document.querySelector('.icon').src = "./imagens/remove.png"
     }
 }
 
